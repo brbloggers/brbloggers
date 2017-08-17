15bb5c0250a744d3e55980b0c8a832e8fe9fe07c
Daniel Falbel <dfalbel@live.com>
2017-08-17 14:21:31 +0000

    infos para o analise-real

---
+++ R/analise-real.R
@@ -0,0 +1,23 @@
+`analise-real` <- list(
+  nome = "Análise Real",
+  url = "https://analisereal.com/feed/",
+  
+  lista = function(){
+    df <- safe_tidyfeed("https://analisereal.com/feed/")
+    df %>%
+      select(
+        feed_title,
+        feed_link,
+        item_title,
+        item_date_published,
+        item_link
+      )
+  },
+  
+  post = function(url){
+    safe_mercury(url)
+  }
+)
+
+# a <- `analise-real`$lista()
+# b <- `analise-real`$post(a$item_link[1])
