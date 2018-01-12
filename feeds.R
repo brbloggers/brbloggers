6dde2917566b1ed073ad1930f6ad2a53c0c5f0de
Daniel Falbel <dfalbel@gmail.com>
2018-01-12 09:15:26 -0200

    atualizar main e feeds e apagar data.rds

--- feeds.R
+++ feeds.R
@@ -1,69 +1,31 @@
-feeds <- list(
-  
-  "NMEC" = list(
-    nome = "NMEC",
-    url = "http://pedrocostaferreira.github.io/blog/tags#R"
-  ),
-  
-  "curso-r" = list(
-    nome = "Curso-R",
-    url = "http://curso-r.com/blog/index.xml"
-  ),
-  
-  "paixao-por-dados" = list(
-    nome = "Paixão por Dados",
-    url = "https://sillasgonzaga.github.io/feed.xml"
-  ),
-  
-  "analise-real" = list(
-    nome = "Análise Real",
-    url = "https://analisereal.com/feed/"
-  ),
-  
-  "dfalbel" = list(
-    nome = "Como faz no R",
-    url = "http://dfalbel.github.io/feed.xml"
-  ),
-  
-  "lurodrigo" = list(
-    nome = "Luiz Rodrigo",
-    url = "http://lurodrigo.com/feed.R_pt.xml"
-  ),
-  
-  "cantinho-do-r" = list(
-    nome = "Cantinho do R",
-    url = "https://cantinhodor.wordpress.com/feed/"
-  ),
-  
-  "IBPAD" = list(
-    nome = "IBPAD",
-    url = "http://www.ibpad.com.br/blog/analise-de-dados/feed/"
-  ),
-  
-  "italocegatta" = list(
-    nome = "Ítalo Cegatta",
-    url = "https://italocegatta.github.io/index.xml"
-  ),
-  
-  "r-python-e-redes" = list(
-    nome = "R Python e Redes",
-    url = "http://neylsoncrepalde.github.io/feed.xml"
-  ),
-  
-  "d-van" = list(
-    nome = "Psiquiatria e neurociências clínicas",
-    url = "https://medium.com/feed/d-van"
-  ),
-  
-  "mind-and-iron" = list(
-    nome = "Mind & Iron",
-    url = "http://ctlente.com/pt/index.xml"
-  ),
-  
-  "jose-guilherme-lopes" = list(
-    nome = "José Guilherme Lopes",
-    url = "http://joseguilhermelopes.com.br/feed/"
-  )
+fls <- dir("R/blogs", full.names = TRUE)
+for(i in fls){
+  source(i)
+}
 
+feeds <- list(
+  "analise-real"               = `analise-real`,
+  "cantinho-do-r"              = `cantinho-do-r`,
+  "curso-r"                    = `curso-r`,
+  "d-van"                      = `d-van`,
+  "dfalbel"                    = `dfalbel`,
+  "IBPAD"                      = `ibpad`,
+  "italocegatta"               = `italocegatta`,
+  "jose-guilherme-lopes"       = `jose-guilherme_lopes`,
+  "lurodrigo"                  = `lurodrigo`,
+  "mind-and-iron"              = `mind-and-iron`,
+  "NMEC"                       = `NMEC`,
+  "paixao-por-dados"           = `paixao-por-dados`,
+  "r-python-e-redes"           = `r-python-e-redes`
 )
+
+
+
+
+
+
+
+
+
+
 
--- main.R
+++ main.R
@@ -1,4 +1,5 @@
-setwd("~/brbloggers/")
+#' Versão nova do arquivo main. 
+#' 
 
 suppressPackageStartupMessages({
   library(purrr)
@@ -8,73 +9,23 @@ suppressPackageStartupMessages({
   library(dplyr)
   library(readr)
   library(xml2)
-  library(slackr)
 })
 
-# secure::add_user("daniel", public_key = secure::local_key())
-# secure::encrypt("MERCURY_KEY", key = "xxxxxxxxxxxxxxxx")
-# secure::encrypt("NETLIFY_BUILD_HOOK", key = "xxxxxxxxxxxxxxx")
-# secure::encrypt("SLACK", key = "")
-# secure::encrypt("SLACK_TOKEN", key = "xxxxxxx")
+# Carregar feeds e funções auxiliares ---------------------------------
 
-# slackr_setup(channel="#brbloggers", api_token = secure::decrypt("SLACK_TOKEN")$key,
-#             incoming_webhook_url=secure::decrypt("SLACK")$key)
-
-message(sprintf("[%s]: Starting update",lubridate::now()))
-# slackr_msg(sprintf("[%s]: Starting update",lubridate::now()))
-
-Sys.setenv(MERCURY_KEY = secure::decrypt("MERCURY_KEY")$key)
-
-
-# Auxiliary functions -----------------------------------------------------
-
-safe_tidyfeed <- possibly(tidyfeed, NA, quiet = TRUE)
-safe_mercury <- possibly(function(x){
-  req <- mercury(x)
-  if(is.null(req$content)){
-    return(NA)
-  } else {
-    return(req$content)
-  }
-}, NA, quiet = FALSE)
-
-fix_file_name <- function(dir, item_link, feed_link){
-  # transform item link
-  item_link <- item_link %>%
-    str_replace(fixed(feed_link), "") %>%
-    str_replace("/$", "") %>%
-    str_extract("/[^/]+$") %>%
-    str_replace_all("%", "-") %>%
-    str_replace_all("[\\(\\)]", "") %>%
-    str_replace_all("^/", "")
-  
-  sprintf(
-    "content/%s/%s", 
-    dir, 
-    item_link
-  )
-}
-
-create_md_header <- function(title, date, blog, original_url){
-  sprintf(
-    '+++\ntitle = "%s"\ndate = "%s"\ncategories = ["%s"]\noriginal_url = "%s"\n+++\n',
-    title,
-    date,
-    blog,
-    original_url
-  )
-}
-
-tratar_titulo <- function(x){
-  stringr::str_replace_all(x, stringr::fixed('"'), "")
-}
-
-# Get all feeds
 source("feeds.R", local = TRUE)
+source("R/utils.R", local = TRUE)
+
+# Iniciar o processamento ---------------------------------------------
+message(sprintf("[%s]: Starting update",lubridate::now()))
+#Sys.setenv(MERCURY_KEY = secure::decrypt("MERCURY_KEY")$key)
+Sys.setenv(MERCURY_KEY = "IB8ejktBd84R2NXpAomsn30bR7MMMel5j2K3UKus")
+
+# Preparar pastas para salvar posts ---------------------------------------
 
 message(sprintf("%02d blogs are watched", length(feeds)))
-# slackr_msg(sprintf("%02d blogs are watched", length(feeds)))
 
+# aqui verificamos se todos os blogs já possuem a sua pasta e etc.
 # verify and create necessary folders
 for(blog in names(feeds)){
   if(!dir.exists(sprintf("content/%s", blog))){
@@ -83,220 +34,29 @@ for(blog in names(feeds)){
   }
 }
 
-# Download feeds ----------------------------------------------------------
+# ler posts antigos que já foram salvos
 
-posts <- readRDS("data/posts.rds")
+posts_salvos <- dir("content", recursive = TRUE, full.names = TRUE)
 
-suppressMessages({
-  all_feeds <- feeds %>%
-    map(~safe_tidyfeed(.x$url))  
-})
-
-# modificações necessárias p/ cada blog
-
-message("Baixando dados do blog curso-r")
-all_feeds$`curso-r` <- try({all_feeds$`curso-r` %>%
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    )})
-
-message("Baixando dados do blog paixao-por-dados")
-all_feeds$`paixao-por-dados` <- try({all_feeds$`paixao-por-dados` %>%
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    )})
-
-message("Baixando dados do blog analise-real")
-all_feeds$`analise-real` <- try({all_feeds$`analise-real` %>%
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    )}) 
-
-message("Baixando dados do blog dfalbel")
-all_feeds$dfalbel <- try({all_feeds$dfalbel %>%
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    )})
-
-message("Baixando dados do blog lurodrigo")
-all_feeds$lurodrigo <- try({all_feeds$lurodrigo %>%
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published = item_date_updated,
-      item_link
-    )})
-
-message("Baixando dados do blog cantinho do R")
-all_feeds$`cantinho-do-r` <- try({all_feeds$`cantinho-do-r` %>%
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    )})
-
-message("Baixando dados do blog IBPAD")
-all_feeds$IBPAD <- try({all_feeds$IBPAD %>% 
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    )})
-
-message("Baixando dados do blog italocegatta")
-all_feeds$italocegatta <- try({all_feeds$italocegatta %>% 
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    ) %>%
-    mutate(item_link = paste0("https://italocegatta.github.io", item_link))
-})
-
-message("Baixando dados do blog r-python-e-redes")
-all_feeds$`r-python-e-redes` <- try({all_feeds$`r-python-e-redes` %>% 
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    )})
-
-message("Baixando dados do blog d-van")
-all_feeds$`d-van` <- try({all_feeds$`d-van` %>%
-    tidyr::unite("category", starts_with("item_category"), sep = " | ") %>%
-    filter(stringr::str_detect(category, stringr::fixed("análise-de-dados"))) %>%
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    )
-})
-
-message("Baixando dados do blog Mind & Iron")
-all_feeds$`mind-and-iron` <- try({all_feeds$`mind-and-iron` %>%
-    filter(item_title != "") %>%
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    )
-})
-
-message("Baixando dados do blog José Guilherme Lopes")
-all_feeds$`jose-guilherme-lopes` <- try({all_feeds$`jose-guilherme-lopes` %>%
-    filter(item_title != "") %>%
-    select(
-      feed_title,
-      feed_link,
-      item_title,
-      item_date_published,
-      item_link
-    )
-})
-
-# feeds$NMEC$url %>%
-#   httr::GET() %>%
-#   httr::content(as = "text") %>%
-#   xml2::read_html() %>%
-#   rvest::html_nodes('.container div')
-# 
-# message("Baixando dados do blog do NMEC")
-# all_feeds$`NMEC` <- all_feeds$`NMEC` %>%
-#   select(
-#     feed_title,
-#     feed_link,
-#     item_title,
-#     item_date_published,
-#     item_link
-#   )
-
-
-
-
-
-
-all_feeds <- all_feeds %>%
-  keep(is.data.frame) %>%
-  map_df(~.x, .id = "blog") %>%
-  filter(
-    item_date_published > (lubridate::today() - 30) | # pegar o conteudo de posts recentes
-      (!blog %in% unique(posts$blog)) # ou de blogs novos
+# obter posts recentes
+new_posts <- feeds %>%
+  map(~.x$lista) %>%
+  map(~safely(.x, otherwise = NULL)) %>%
+  map_df(~.x()$result, .id = "blog") %>%
+  mutate(
+    fname = paste0(fix_file_name(
+      blog, 
+      item_link, 
+      feed_link
+    ), ".md")
   ) %>%
-  mutate(item_content = map_chr(item_link, safe_mercury))
+  filter(! fname %in% posts_salvos) %>%
+  mutate(
+    post_fun = map(feeds[blog], ~.x$post),
+    item_content = map2_chr(post_fun, item_link, ~.x(.y))
+  ) %>%
+  select(-post_fun, -fname)
 
-# tweaks para o IBPAD
-try({
-  feed_ibpad <- read_xml("http://www.ibpad.com.br/blog/analise-de-dados/feed/") %>%
-    rvest::xml_nodes("item")
-  dados_ibpad <- data_frame(
-    item_link = feed_ibpad %>% xml_child("link") %>% xml_text(),
-    item_content = feed_ibpad %>% xml_child("content:encoded") %>% xml_text()
-  )
-  
-  # retornar dados do IBPAD
-  all_feeds <- bind_rows(
-    all_feeds %>% filter(blog != "IBPAD"),
-    all_feeds %>% 
-      filter(blog == "IBPAD") %>% 
-      select(-item_content) %>% 
-      left_join(dados_ibpad, by = "item_link")
-  )
-})
-
-# Decide which posts will be written
-new_posts <- setdiff(all_feeds, posts)
-
-message(sprintf("%03d new posts", nrow(new_posts)))
-# slackr_msg(sprintf("%03d new posts", nrow(new_posts)))
-if(nrow(new_posts) > 0){
-  message(
-    sprintf("%s\n", 
-            paste0(new_posts$item_title, "já existe em posts?",  
-                   new_posts$item_title %in% posts$item_title
-            )
-    )
-  )
-  
-  # slackr_msg(
-  #  sprintf("%s\n", 
-  #          paste0(new_posts$item_title, "já existe em posts?",  
-  #                 new_posts$item_title %in% posts$item_title
-  #          )
-  #  )
-  #)
-}
-
-posts <- bind_rows(posts, new_posts)
-saveRDS(posts, "data/posts.rds")
 
 # Write feeds to directory ------------------------------------------------
 if(nrow(new_posts) > 0){
@@ -352,15 +112,14 @@ if(nrow(new_posts) > 0){
 
 # adicionar perguntas mais recentes do stack-overflow
 
-all_sopt <- readRDS("data/sopt.rds")
+lista_sopt <- dir("content/sopt", full.names = TRUE)
+
 sopt <- tidyfeed("https://pt.stackoverflow.com/feeds/tag?tagnames=r&sort=newest") %>%
-  select(-feed_last_updated)
+  select(-feed_last_updated) %>%
+  mutate(fname = sprintf("content/sopt/%s.md", str_extract(item_link, "[^/]*$")))
 
-new_sopt <- setdiff(sopt, all_sopt) %>%
-  filter(item_date_updated > max(all_sopt$item_date_updated))
-
-all_sopt <- bind_rows(all_sopt, new_sopt)
-saveRDS(all_sopt, "data/sopt.rds")
+new_sopt <- sopt %>%
+  filter(!fname %in% lista_sopt)
 
 if(nrow(new_sopt) > 0){
   message("writing new posts")
@@ -380,15 +139,3 @@ if(nrow(new_sopt) > 0){
 
 message(sprintf("[%s]: Finished",lubridate::now()))
 # slackr_msg(sprintf("[%s]: Finished",lubridate::now()))
-
-
-
-
-
-
-
-
-
-
-
-
